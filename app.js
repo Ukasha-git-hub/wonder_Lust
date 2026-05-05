01699f7c2147793a35a4b1e32144c5a7e18604be
ukasha khursheed <ukashakhursheed@gmail.com>
2026-05-05 22:16:22 +0500

    fix start file

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ require("dotenv").config();
 
 const express = require("express");
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 3000;
+
 const mongoose = require("mongoose");
 // const listing = require("./models/listing.js");
 const path = require("path");
